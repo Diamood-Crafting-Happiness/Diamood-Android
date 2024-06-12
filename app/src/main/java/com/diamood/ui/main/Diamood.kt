74073c82df2dbd3e24fe234fb7f2b9605a62b372
Voro Ferrer <ferrer.peris.voro@gmail.com>
2024-06-12 17:40:16 +0200

    Add different icons for debug/release

--- app/src/main/java/com/diamood/ui/main/Diamood.kt
+++ app/src/main/java/com/diamood/ui/main/Diamood.kt
@@ -1,9 +1,22 @@
 package com.diamood.ui.main
 
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun Diamood() {
-    val navController = rememberNavController()
+    Text(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .wrapContentSize(),
+        text = "Diamood",
+        textAlign = TextAlign.Center,
+    )
 }
+
