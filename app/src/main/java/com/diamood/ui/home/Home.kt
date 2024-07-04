46721ff53149744e091067f5c65eb9048a71a750
Voro Ferrer Peris <73641690+VoroFP@users.noreply.github.com>
2024-07-04 12:47:31 +0200

    [Added] Add Home title (#7)

--- app/src/main/java/com/diamood/ui/home/Home.kt
+++ app/src/main/java/com/diamood/ui/home/Home.kt
@@ -3,23 +3,19 @@ package com.diamood.ui.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.diamood.ui.home.buttons.HomeButtons
 import com.diamood.ui.home.buttons.HomeDirection
+import com.diamood.ui.home.title.HomeTitle
 import com.diamood.ui.home.tutorial.HomeTutorial
 
 
 @Composable
 fun Home(navigate: (HomeDirection) -> Unit) {
     Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = "Title")
-        }
+        HomeTitle()
         HomeTutorial()
         HomeButtons(navigate)
     }
