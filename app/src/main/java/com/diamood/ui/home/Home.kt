b0eb0702016053a74c34dc9c16b884312579cbd4
Voro Ferrer <ferrer.peris.voro@gmail.com>
2024-10-01 17:43:57 +0200

    Add Composables

--- app/src/main/java/com/diamood/ui/home/Home.kt
+++ app/src/main/java/com/diamood/ui/home/Home.kt
@@ -6,18 +6,40 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.diamood.data.main.routes.Routes.LoggedHomeRoute
+import com.diamood.data.main.routes.Routes.TutorialRoute
 import com.diamood.ui.home.buttons.HomeButtons
 import com.diamood.ui.home.buttons.HomeDirection
+import com.diamood.ui.home.logged.HomeLogged
 import com.diamood.ui.home.title.HomeTitle
 import com.diamood.ui.home.tutorial.HomeTutorial
+import com.diamood.viewmodels.home.HomeViewModel
 
 
 @Composable
 fun Home(navigate: (HomeDirection) -> Unit) {
-    Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
-        HomeTitle()
-        HomeTutorial()
-        HomeButtons(navigate)
+    val viewModel: HomeViewModel = hiltViewModel<HomeViewModel>()
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = viewModel.route, modifier = Modifier) {
+        composable<TutorialRoute>(typeMap = TutorialRoute.typeMap) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                HomeTitle()
+                HomeTutorial()
+                HomeButtons(navigate)
+            }
+        }
+
+        composable<LoggedHomeRoute>(typeMap = LoggedHomeRoute.typeMap) {
+            HomeLogged(navController = navController)
+        }
     }
 }
 
