f930bb0a515a21fc986b5375c95a84391c88c955
Voro Ferrer Peris <73641690+VoroFP@users.noreply.github.com>
2024-10-24 23:29:43 +0200

    Change navigation event (#16)

--- app/src/main/java/com/diamood/ui/login/Login.kt
+++ app/src/main/java/com/diamood/ui/login/Login.kt
@@ -1,10 +1,15 @@
 package com.diamood.ui.login
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -18,6 +23,7 @@ import com.diamood.ui.login.screen.CountryListScreen
 import com.diamood.ui.login.screen.LoginScreen
 import com.diamood.viewmodels.login.CountryListViewModel
 import com.diamood.viewmodels.login.LoginInputViewModel
+import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun Login(onNavigate: (Routes) -> Unit) {
@@ -31,17 +37,39 @@ fun Login(onNavigate: (Routes) -> Unit) {
         }
 
         composable<CountryRoute>(typeMap = CountryRoute.typeMap) {
-            CountryListScreen(countryListViewModel) {
-                loginInputViewModel.onCountrySelected(it)
-                navController.popBackStack()
+            ObserveAsEvents(flow = countryListViewModel.navigationEventsChannelFlow) { event ->
+                when(event) {
+                    is LoginNavigationEvent.NavigateToLogin -> navController.popBackStack()
+                }
+            }
+
+            CountryListScreen(countryListViewModel) { country ->
+                country?.let {
+                    loginInputViewModel.onCountrySelected(it)
+                }
             }
         }
     }
 }
 
+@Composable
+private fun <T> ObserveAsEvents(flow: Flow<T>, onEvent: (T) -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(flow, lifecycleOwner.lifecycle) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collect(onEvent)
+        }
+    }
+
+}
+
 val loginBackground = Brush.horizontalGradient(
     colors = listOf(SecondaryLight, PrimaryLight),
 )
+
+sealed interface LoginNavigationEvent {
+    object NavigateToLogin: LoginNavigationEvent
+}
 
 @Preview
 @Composable
--- app/src/main/java/com/diamood/ui/login/screen/CountryListScreen.kt
+++ app/src/main/java/com/diamood/ui/login/screen/CountryListScreen.kt
@@ -1,6 +1,7 @@
 package com.diamood.ui.login.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,24 +11,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.diamood.api.country.CountryRepositoryFakeImpl
 import com.diamood.data.login.Country
 import com.diamood.domain.login.GetCountryListUseCase
+import com.diamood.theme.PrimaryLight
 import com.diamood.viewmodels.login.CountryListViewModel
 
 @Composable
-fun CountryListScreen(viewModel: CountryListViewModel, onCountrySelected: (Country) -> Unit) {
+fun CountryListScreen(viewModel: CountryListViewModel, onCountrySelected: (Country?) -> Unit) {
     val searchText by viewModel.searchText.collectAsState()
     val countries by viewModel.countries.collectAsState()
 
@@ -36,10 +42,24 @@ fun CountryListScreen(viewModel: CountryListViewModel, onCountrySelected: (Count
             .fillMaxSize()
             .padding(16.dp)
     ) {
+        Text(
+            text = "Cancelar",
+            fontSize = 18.sp,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .clickable { viewModel.cancelSelection() },
+            textAlign = TextAlign.Center,
+            color = PrimaryLight,
+        )
         TextField(
             value = searchText,
             onValueChange = viewModel::onSearchTextChange,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            colors = TextFieldDefaults.colors(unfocusedIndicatorColor = Color.Transparent),
+            shape = RoundedCornerShape(8.dp)
+
         )
         Spacer(modifier = Modifier.height(16.dp))
         LazyColumn(
