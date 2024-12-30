a73fb2953f20ffb7e79a96e3e6f3e49431943dcc
Voro Ferrer <ferrer.peris.voro@gmail.com>
2024-12-30 12:32:28 +0100

    Make test green

--- app/src/main/java/com/diamood/viewmodels/login/LoginInputViewModel.kt
+++ app/src/main/java/com/diamood/viewmodels/login/LoginInputViewModel.kt
@@ -8,8 +8,17 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
+sealed class LoginState() {
+    data object Loading : LoginState()
+    data object InProgress : LoginState()
+    data object Completed : LoginState()
+}
+
 @HiltViewModel
 class LoginInputViewModel @Inject internal constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow<LoginState>(LoginState.InProgress)
+    val uiState = _uiState.asStateFlow()
 
     private val _phoneInfo = MutableStateFlow(
         PhoneInfo(
@@ -29,5 +38,9 @@ class LoginInputViewModel @Inject internal constructor() : ViewModel() {
 
     fun onPhoneChanged(phone: String) {
         _phoneInfo.value = _phoneInfo.value.copy(number = phone)
+    }
+
+    fun onSendSMSClicked() {
+        _uiState.value = LoginState.Loading
     }
 }
