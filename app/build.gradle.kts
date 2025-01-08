addc8fd9bd5b5dce9c1b5a846d14ab9b552d11a3
Voro Ferrer Peris <73641690+VoroFP@users.noreply.github.com>
2025-01-08 12:39:42 +0100

    [Changed] Update project (#28)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.diamood"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.diamood"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
